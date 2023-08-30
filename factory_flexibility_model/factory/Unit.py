eeaa34506baf2a5f8edabf2e464ae28619587fa2
smsikamm <simon.kammerer@tu-dortmund.de>
2023-08-30 09:06:18 +0200

    Added more suffisticated unit handling: - Unit-class: specifies a physical unit with all possible magnitudes - Flowtype-class: specifies a flow, consisting of a description and a Unit - DataValue-class: specifies a physical quantity consisting of a numeric value and a Flowtype

---
+++ factory_flexibility_model/factory/Unit.py
@@ -0,0 +1,96 @@
+import logging
+
+import numpy as np
+
+
+class Unit:
+    def __init__(self, *, key: str = "unit"):
+        self.key = "unspecified_unit"
+        self.conversion_factor = 1
+        self.magnitudes = np.array([1, 1000, 1000000, 1000000000, 1000000000000])
+        self.units_flow = [" kWh", " MWh", " GWh", " TWh", " PWh"]
+        self.units_flowrate = [" kW", " MW", " GW", " TW", " PW"]
+        self.quantity_type = "Energy"
+
+        if key == "kW" or key == "energy":
+            self.conversion_factor = 1
+            self.magnitudes = np.array([1, 1000, 1000000, 1000000000, 1000000000000])
+            self.units_flow = [" kWh", " MWh", " GWh", " TWh", " PWh"]
+            self.units_flowrate = [" kW", " MW", " GW", " TW", " PW"]
+            self.resource_type = "Energy"
+        elif key == "kg" or key == "mass":
+            self.conversion_factor = 1
+            self.magnitudes = np.array([1, 1000, 1000000, 1000000000, 1000000000000])
+            self.units_flow = [" kg", " t", " kt", " mt", " gt"]
+            self.units_flowrate = [" kg/h", " t/h", " kt/h", " mt/h", " gt/h"]
+            self.resource_type = "Material"
+        elif key == "J":
+            self.conversion_factor = 1 / 3.6
+            self.magnitudes = np.array([1, 1000, 1000000, 1000000000])
+            self.units_flow = [" MJ", " GJ", " TJ", " PJ"]
+            self.units_flowrate = [" MJ/s", " GJ/s", " TJ/s", " PJ/s"]
+            self.resource_type = "Energy"
+        else:
+            if not key == "unit":
+                logging.warning(
+                    f"{key} is unknown as a unit prefix. The unit is created without further specification"
+                )
+            self.magnitudes = np.array([1, 1000, 1000000, 1000000000, 1000000000000])
+            self.units_flow = [" Units", "k Units", "m. Units", "bn. Units", "T. Units"]
+            self.units_flowrate = [
+                " Units/h",
+                "k Units/h",
+                "m. Units/h",
+                "bn. Units/h",
+                "trillion Units/h",
+            ]
+            self.resource_type = "unspecified"
+
+    def get_value_expression(
+        self, value: float, quantity_type: str, *, digits: int = 2
+    ) -> str:
+        """
+        This function takes a numeric value, the information wether it describes a flow or a flowrate and optionally a number of rounding digits.
+        It returns a string describing the data in the correct unit and magnitude.
+        F.e: 15000000 + "flow" -> "1.5GW"
+        :param value: [float] a numeric value in the base unit (kW/kg)
+        :param type: [string] "Flow" or "Flowrate"
+        :param digits: [int] Requested number of digits behind the decimal point; Standard: 2
+        :return: [string] Description of the value with correct magnitude and unit
+        """
+
+        magnitude = np.argmin(abs(value - self.magnitudes))
+
+        if quantity_type == "flow":
+            return f"{round(value/self.magnitudes[magnitude], digits)}{self.units_flow[magnitude]}"
+        elif quantity_type == "flowrate":
+            return f"{round(value/self.magnitudes[magnitude], digits)}{self.units_flowrate[magnitude]}"
+        else:
+            logging.error(
+                f"The type '{quantity_type}' is an invalid argument for the .get_value_expression()-function. Valid types are 'flow' and 'flowrate'!"
+            )
+            return f"{value} [UNIT ERROR]"
+
+    def is_energy(self) -> bool:
+        """
+        :return: True, if the unit describes an energy value
+        """
+        return self.resource_type == "Energy"
+
+    def is_material(self) -> bool:
+        """
+        :return: True, if the unit describes a material value
+        """
+        return self.resource_type == "Material"
+
+    def get_unit_flow(self) -> str:
+        """
+        :return: [string] Standard unit description of the flow
+        """
+        return self.units_flow[0]
+
+    def get_unit_flowrate(self) -> str:
+        """
+        :return: [string] Standard unit description of the flowrate
+        """
+        return self.units_flowrate[0]
