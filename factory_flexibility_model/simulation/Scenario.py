94f8bc16e42c8a187e5de69faf489f5f079e9e2e
smsikamm <simon.kammerer@tu-dortmund.de>
2023-08-31 16:32:13 +0200

    - Added timeseries and parameter import from .txt files

--- factory_flexibility_model/simulation/Scenario.py
+++ factory_flexibility_model/simulation/Scenario.py
@@ -18,9 +18,17 @@ class Scenario:
         # set timefactor
         self.timefactor = timefactor
 
-        # read in parameter.txt
+        # read in parameters.txt
         if parameter_file is not None:
             self.import_parameters(parameter_file)
+        else:
+            self.parameters = {}
+
+        # read in timeseries.txt
+        if timeseries_file is not None:
+            self.import_timeseries(timeseries_file)
+        else:
+            self.timeseries = {}
 
     def import_parameters(self, parameter_file: str) -> bool:
         """
@@ -43,5 +51,37 @@ class Scenario:
         except:
             logging.error(
                 f"The given file is not a valid parameters.txt-config file! ({parameter_file})"
+            )
+            raise Exception
+
+    def import_timeseries(self, timeseries_file: str) -> bool:
+        """
+        This function opens the .txt file given as "timeseries_file" and returns the contained timeseries as a dictionary with one key: [array] pair per timeseries specified
+        :param timeseries_file: [string] Path to a .txt file containing the key: [array] pairs
+        :return: [boolean] True if import was successfull
+        """
+
+        # initialize self.parameters
+        self.timeseries = {}
+
+        try:
+            # open the given file
+            with open(timeseries_file) as file:
+                # iterate over all lines in the file
+                for line in file:
+                    # parse current line
+                    items = line.split("\t")
+
+                    # first item of the line is the key
+                    key = items[0]
+
+                    # remaining line is the value array
+                    values = [float(x.replace(",", ".")) for x in items[1:]]
+
+                    # add entry to parameters-dict
+                    self.timeseries[key] = values
+        except:
+            logging.error(
+                f"The given file is not a valid timeseries.txt-config file! ({timeseries_file})"
             )
             raise Exception
