d6cb177ca4f603b11bdac513cd336bd21c1ef25b
smsikamm <simon.kammerer@tu-dortmund.de>
2023-09-05 11:10:12 +0200

    NEW FILE: This file by now only contains the function create_session_folder(), which initializes a new session directory

---
+++ factory_flexibility_model/io/session.py
@@ -0,0 +1,34 @@
+# this file contains all scripts used to create new session directories and to evaluate, if a directory is a valid session folder
+
+# IMPORTS
+import os
+
+
+def create_session_folder(root_folder, session_name: str = "New Session"):
+    """
+    This function initializes a session folder with all required files and subfolders
+    :param root_folder: [str] User given path where the new session folder shall be created
+    :session_name: [str] Name for the new session == Name of the subfolder to be created
+    """
+
+    # Create path to the new session folder
+    subfolder_path = os.path.join(root_folder, session_name)
+
+    # create list of files to be initialized
+    files = [
+        "parameters.txt",
+        "timeseries.txt",
+        "flowtypes.txt",
+        "units.txt",
+        "Layout.factory",
+    ]
+
+    # create empty files
+    for file in files:
+        with open(os.path.join(subfolder_path, file), "w") as f:
+            pass
+
+    # create a subfolder for simulations
+    simulations_path = os.path.join(subfolder_path, "simulations")
+    if not os.path.exists(simulations_path):
+        os.makedirs(simulations_path)
