f7de3eeca77ab40091a351307c49bf45bc638fb6
smsikamm <simon.kammerer@tu-dortmund.de>
2023-09-20 11:57:37 +0200

    refactor: the functions of the converter ratio dialog are now directly called from the gui instead of being routed through the app main class

--- factory_flexibility_model/ui/dialogs/dialog_converter_ratios.py
+++ factory_flexibility_model/ui/dialogs/dialog_converter_ratios.py
@@ -6,8 +6,6 @@ from kivy.uix.boxlayout import BoxLayout
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.list import TwoLineAvatarIconListItem
 
-# LAYOUTS
-Builder.load_file(r"factory_flexibility_model\ui\dialogs\dialog_converter_ratios.kv")
 
 # CLASSES
 class dialog_converter_ratios(BoxLayout):
@@ -22,186 +20,21 @@ class TextfieldCheckboxIconListItem(TwoLineAvatarIconListItem):
     connection_key = StringProperty()
 
 
+from factory_flexibility_model.ui.utility.converter_ratio_dialog_functions import (
+    update_bilance_calculation,
+    update_connection_lists,
+)
+
+
 # FUNCTIONS
-def save_converter_ratios(app):
-    """
-    This function goes through all the list items within the converter ratio definition dialog and writes the user given values for the weights from the GUI into the corresponding connections within the blueprint.
-    :param app: kivy-app-object that the dialog is being called from
-    """
-
-    # make sure that the current values are valid
-    if not app.dialog.bilance_valid:
-        app.show_info_popup("The given ratios are invalid!")
-        return
-
-    # iterate over all connections and write their weighting factors into the blueprint
-    for input in app.dialog.content_cls.ids.list_converter_inputs_energy.children:
-        app.blueprint.connections[input.connection_key]["weight_sink"] = float(
-            input.ids.list_item_textfield.text
-        )
-    for input in app.dialog.content_cls.ids.list_converter_inputs_mass.children:
-        app.blueprint.connections[input.connection_key]["weight_sink"] = float(
-            input.ids.list_item_textfield.text
-        )
-    for output in app.dialog.content_cls.ids.list_converter_outputs_energy.children:
-        app.blueprint.connections[output.connection_key]["weight_source"] = float(
-            output.ids.list_item_textfield.text
-        )
-        app.blueprint.connections[output.connection_key][
-            "to_losses"
-        ] = output.ids.list_item_checkbox.active
-    for output in app.dialog.content_cls.ids.list_converter_outputs_mass.children:
-        app.blueprint.connections[output.connection_key]["weight_source"] = float(
-            output.ids.list_item_textfield.text
-        )
-        app.blueprint.connections[output.connection_key][
-            "to_losses"
-        ] = output.ids.list_item_checkbox.active
-
-    # close the dialog
-    app.close_dialog()
-
-
-def update_bilance_calculation(app):
-    """
-    This function calculates the sum of inputs and outputs for energy and material normalized to the standard units and writes the results into the textlabels within the dialog.
-    :param app: kivy-app-object that the dialog is being called from
-    """
-    # Assume the bilance to be valid
-    app.dialog.bilance_valid = True
-    app.dialog.content_cls.ids.label_energy_out.theme_text_color = "Primary"
-    app.dialog.content_cls.ids.label_mass_out.theme_text_color = "Primary"
-
-    # Calculate energy input sum
-    sum_energy_input = 0
-    for input in app.dialog.content_cls.ids.list_converter_inputs_energy.children:
-        try:
-            sum_energy_input += app.blueprint.connections[input.connection_key][
-                "flowtype"
-            ].unit.conversion_factor * float(input.ids.list_item_textfield.text)
-            input.ids.list_item_textfield.error = False
-        except:
-            # error detected...
-            input.ids.list_item_textfield.error = True
-            app.dialog.bilance_valid = False
-
-    # Calculate energy output sum
-    sum_energy_output = 0
-    loss_connection = None
-    for output in app.dialog.content_cls.ids.list_converter_outputs_energy.children:
-        try:
-            # if the connection is responsible for losses -> save it for later and continue
-            if output.ids.list_item_checkbox.active:
-                loss_connection = output
-                continue
-
-            # add the influence of the current connection to the output sum
-            sum_energy_output += app.blueprint.connections[output.connection_key][
-                "flowtype"
-            ].unit.conversion_factor * float(output.ids.list_item_textfield.text)
-            output.ids.list_item_textfield.error = False
-        except:
-            # error detected...
-            app.dialog.bilance_valid = False
-            output.ids.list_item_textfield.error = True
-
-    if loss_connection is not None:
-        loss_connection.ids.list_item_textfield.text = str(
-            round(
-                (sum_energy_input - sum_energy_output)
-                / app.blueprint.connections[loss_connection.connection_key][
-                    "flowtype"
-                ].unit.conversion_factor,
-                3,
-            )
-        )
-        if sum_energy_input - sum_energy_output < 0:
-            # error detected...
-            app.dialog.bilance_valid = False
-            loss_connection.ids.list_item_textfield.error = True
-        else:
-            sum_energy_output = sum_energy_input
-
-    # Write results to GUI
-    app.dialog.content_cls.ids.label_energy_in.text = f"{round(sum_energy_input,3)} kWh"
-    if sum_energy_input == sum_energy_output:
-        app.dialog.content_cls.ids.label_energy_out.text = (
-            f"{round(sum_energy_output,3)} kWh"
-        )
-    elif sum_energy_input > sum_energy_output:
-        app.dialog.content_cls.ids.label_energy_out.text = f"{round(sum_energy_output,3)} kWh (+ {round(sum_energy_input - sum_energy_output,3)} kWh losses)"
-    else:
-        app.dialog.content_cls.ids.label_energy_out.text = f"{round(sum_energy_output,3)} kWh ({round(sum_energy_output - sum_energy_input,3)} kWh too much!)"
-        app.dialog.content_cls.ids.label_energy_out.theme_text_color = "Error"
-        app.dialog.bilance_valid = False
-
-    # Calculate mass input sum
-    sum_mass_input = 0
-    for input in app.dialog.content_cls.ids.list_converter_inputs_mass.children:
-        try:
-            sum_mass_input += app.blueprint.connections[input.connection_key][
-                "flowtype"
-            ].unit.conversion_factor * float(input.ids.list_item_textfield.text)
-            input.ids.list_item_textfield.error = False
-        except:
-            # error detected...
-            app.dialog.bilance_valid = False
-            input.ids.list_item_textfield.error = True
-    # Calculate mass output sum
-    sum_mass_output = 0
-    for output in app.dialog.content_cls.ids.list_converter_outputs_mass.children:
-        try:
-            # if the connection is responsible for losses -> save it for later and continue
-            if output.ids.list_item_checkbox.active:
-                loss_connection = output
-                continue
-
-            # add the influence of the current connection to the output sum
-            sum_mass_output += app.blueprint.connections[output.connection_key][
-                "flowtype"
-            ].unit.conversion_factor * float(output.ids.list_item_textfield.text)
-            output.ids.list_item_textfield.error = False
-        except:
-            # error detected...
-            app.dialog.bilance_valid = False
-            output.ids.list_item_textfield.error = True
-
-    if loss_connection is not None:
-        loss_connection.ids.list_item_textfield.text = str(
-            round(
-                (sum_mass_input - sum_mass_output)
-                / app.blueprint.connections[loss_connection.connection_key][
-                    "flowtype"
-                ].unit.conversion_factor,
-                3,
-            )
-        )
-        if sum_mass_input - sum_mass_output < 0:
-            # error detected...
-            app.dialog.bilance_valid = False
-            loss_connection.ids.list_item_textfield.error = True
-        else:
-            sum_mass_output = sum_mass_input
-
-    # write results to GUI
-    app.dialog.content_cls.ids.label_mass_in.text = f"{round(sum_mass_input,3)} kg"
-    if sum_mass_input == sum_mass_output:
-        app.dialog.content_cls.ids.label_mass_out.text = (
-            f"{round(sum_mass_output,3)} kg"
-        )
-    elif sum_mass_input > sum_mass_output:
-        app.dialog.content_cls.ids.label_mass_out.text = f"{round(sum_mass_output,3)} kg (+ {round(sum_mass_input - sum_mass_output,3)} kg losses)"
-    else:
-        app.dialog.content_cls.ids.label_mass_out.text = f"{round(sum_mass_output,3)} kg ({round(sum_mass_output - sum_mass_input,3)} kg too much!)"
-        app.dialog.content_cls.ids.label_mass_out.theme_text_color = "Error"
-        app.dialog.bilance_valid = False
-
-
 def show_converter_ratio_dialog(app):
     """
     This function opens the dialog to specify converter input/output ratios.
     :param app: kivy-app-object that the dialog is being called from
     """
+    Builder.load_file(
+        r"factory_flexibility_model\ui\dialogs\dialog_converter_ratios.kv"
+    )
 
     app.dialog = MDDialog(
         title=f"{app.selected_asset['name']}: Input/Output Ratios",
@@ -210,83 +43,9 @@ def show_converter_ratio_dialog(app):
         auto_dismiss=False,
     )
 
-    # display the icon of the current converter in the middle of the dialog
-    # app.dialog.content_cls.ids.image_converter_top.source = app.selected_asset["GUI"]["icon"]
-    # app.dialog.content_cls.ids.image_converter_bottom.source = app.selected_asset["GUI"]["icon"]
-
     app.dialog.size_hint = (None, None)
     app.dialog.width = dp(1050)
     app.dialog.height = dp(1000)
     update_connection_lists(app)
     update_bilance_calculation(app)
     app.dialog.open()
-
-
-def update_connection_lists(app):
-    """
-    This function updates the lists of incoming and outgoing connections within the component_ratio dialog, filling in all the connections from/to the currently selected converter asset.
-    """
-
-    # clear the current lists
-    app.dialog.content_cls.ids.list_converter_inputs_energy.clear_widgets()
-    app.dialog.content_cls.ids.list_converter_outputs_energy.clear_widgets()
-    app.dialog.content_cls.ids.list_converter_inputs_mass.clear_widgets()
-    app.dialog.content_cls.ids.list_converter_outputs_mass.clear_widgets()
-
-    # track, if energy and material loss connections have been specified
-
-    # iterate over all connections within the current factory blueprint
-    for connection in app.blueprint.connections.values():
-
-        # check if the connection is an input to the selected converter:
-        if connection["to"] == app.selected_asset["key"]:
-
-            # create list item for the connection
-            item = TextfieldIconListItem(
-                text=app.blueprint.components[connection["from"]]["name"],
-                secondary_text=connection["flowtype"].name,
-                connection_key=connection["key"],
-                divider_color=(1, 1, 1, 1),
-            )
-            item.ids.list_item_textfield.helper_text = connection[
-                "flowtype"
-            ].unit.get_unit_flow()
-            item.ids.list_item_textfield.text = str(connection["weight_sink"])
-            # add list item to the correct  input_list
-            if connection["flowtype"].unit.is_energy():
-                app.dialog.content_cls.ids.list_converter_inputs_energy.add_widget(item)
-            elif connection["flowtype"].unit.is_mass():
-                app.dialog.content_cls.ids.list_converter_inputs_mass.add_widget(item)
-
-        # check if the connection is an output of the selected converter:
-        if connection["from"] == app.selected_asset["key"]:
-            # create list item for the connection
-            item = TextfieldCheckboxIconListItem(
-                text=app.blueprint.components[connection["to"]]["name"],
-                secondary_text=connection["flowtype"].name,
-                connection_key=connection["key"],
-                divider_color=(1, 1, 1, 1),
-            )
-            item.ids.list_item_textfield.helper_text = connection[
-                "flowtype"
-            ].unit.get_unit_flow()
-            item.ids.list_item_textfield.text = str(connection["weight_source"])
-
-            # add list item to the output_list
-            if connection["flowtype"].unit.is_energy():
-                # add the checkbox to the correct group
-                item.ids.list_item_checkbox.group = "energy"
-                # check, if the current connection is meant to deduct losses
-                if connection["to_losses"]:
-                    item.ids.list_item_checkbox.active = True
-                app.dialog.content_cls.ids.list_converter_outputs_energy.add_widget(
-                    item
-                )
-
-            elif connection["flowtype"].unit.is_mass():
-                # add the checkbox to the correct group
-                item.ids.list_item_checkbox.group = "mass"
-                # check, if the current connection is meant to deduct losses
-                if connection["to_losses"]:
-                    item.ids.list_item_checkbox.active = True
-                app.dialog.content_cls.ids.list_converter_outputs_mass.add_widget(item)
