f3fcdedb5b56afd9e82102f943d974666145d89e
smsikamm <simon.kammerer@tu-dortmund.de>
2023-09-21 17:01:32 +0200

    feat: import_timeseries is now adding new timeseries instead of overwriting the whole list when called

--- factory_flexibility_model/ui/utility/timeseries_handling.py
+++ factory_flexibility_model/ui/utility/timeseries_handling.py
@@ -6,7 +6,8 @@ from kivymd.uix.snackbar import Snackbar
 
 def import_timeseries_xlsx(app):
     """
-    This function lets the user choose an excel file containing timeseries and imports all of them to the tool
+    This function lets the user choose an excel file containing timeseries and imports all of them to the current session.
+    If keys are redundant, the timeseries from the file are enumerated to make them unique.
     """
 
     # ask for filename
@@ -15,7 +16,16 @@ def import_timeseries_xlsx(app):
 
     if not filepath == "":
         # import the excel sheet
-        app.timeseries = pd.read_excel(filepath)
+        imported_timeseries = pd.read_excel(filepath)
 
-    # inform the user
-    Snackbar(text=f"{len(app.timeseries.keys())} new timeseries imported").open()
+        for timeseries_key, timeseries_values in imported_timeseries.items():
+            # make sure, that the key is unique
+            if timeseries_key in app.timeseries.keys():
+                index = 1
+                while f"{timeseries_key}_{index}" in app.timeseries.keys():
+                    index += 1
+                timeseries_key = f"{timeseries_key}_{index}"
+            app.timeseries[timeseries_key] = timeseries_values
+
+        # inform the user
+        Snackbar(text=f"{len(app.timeseries.keys())} new timeseries imported").open()
