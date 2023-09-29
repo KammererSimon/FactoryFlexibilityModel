599c7ac01cbf8f48195cbd757df5b3ef5ea5b1b8
smsikamm <simon.kammerer@tu-dortmund.de>
2023-09-29 17:11:34 -0400

    feat: integrated a more suffisticated system to display the correct units in the parameter list

--- factory_flexibility_model/ui/utility/update_config_tab.py
+++ factory_flexibility_model/ui/utility/update_config_tab.py
@@ -18,31 +18,31 @@ converter_parameters = {
     "power_max": {
         "text": "Maximum Operating Point",
         "description": "Maximum Conversion Rate per Timestep",
-        "unit_type": "float",
+        "unit_type": "flowrate",
     },
     "power_min": {
         "text": "Minimum Operating Point",
         "description": "Minimum Conversion Rate per Timestep",
-        "unit_type": "float",
+        "unit_type": "flowrate",
     },
     "availabiity": {
         "text": "Availability",
-        "description": "Availability of Pmax [%]",
+        "description": "Availability of Pmax",
         "unit_type": "%",
     },
     "max_pos_ramp_power": {
         "text": "Positive Ramping",
-        "description": "Maximum Ramp Up [Conversions/h²]",
-        "unit_type": "flowrate",
+        "description": "Maximum Ramp Up",
+        "unit_type": "ramping",
     },
     "max_neg_ramp_power": {
         "text": "Negative Ramping",
-        "description": "Maximum Ramp Down [Conversions/h²]",
-        "unit_type": "flowrate",
+        "description": "Maximum Ramp Down",
+        "unit_type": "ramping",
     },
     "eta_max": {
         "text": "Best Efficiency",
-        "description": "Maximum Efficiency at nominal operting Point [%]",
+        "description": "Maximum Efficiency at nominal operating Point",
         "unit_type": "%",
     },
     "power_nominal": {
@@ -53,19 +53,19 @@ converter_parameters = {
     "delta_eta_high": {
         "text": "Upper Efficiency Drop",
         "description": "Rate of efficiency drop when exceeding nominal operating point",
-        "unit_type": "exception",
+        "unit_type": "efficiency_drop",
     },
     "delta_eta_low": {
         "text": "Lower Efficiency Drop",
         "description": "Rate of efficiency drop when operating below nominal operating point",
-        "unit_type": "exception",
+        "unit_type": "efficiency_drop",
     },
 }
 deadtime_parameters = {
     "delay": {
         "text": "Delay",
         "description": "Delay Number of Timesteps",
-        "unit_type": "integer",
+        "unit_type": "timesteps",
     }
 }
 schedule_parameters = {
@@ -98,22 +98,22 @@ sink_parameters = {
     },
     "cost": {
         "text": "Cost",
-        "description": "Cost per Unit of Utilization in [€]",
+        "description": "Cost per Unit of Utilization",
         "unit_type": "currency",
     },
     "revenue": {
         "text": "Revenue",
-        "description": "Revenue per Unit of Utilization in [€]",
+        "description": "Revenue per Unit of Utilization",
         "unit_type": "currency",
     },
     "co2_emission_per_unit": {
         "text": "Emissions Caused",
-        "description": "Caused Emissions in [kg CO2/unit]",
+        "description": "Caused Emissions",
         "unit_type": "emissions",
     },
     "co2_refund_per_unit": {
         "text": "Emissions Saved",
-        "description": "Saved Emissions in [kg CO2/unit]",
+        "description": "Saved Emissions]",
         "unit_type": "emissions",
     },
 }
@@ -140,17 +140,17 @@ source_parameters = {
     },
     "cost": {
         "text": "Cost",
-        "description": "Cost per Unit of Utilization in [€]",
+        "description": "Cost per Unit of Utilization",
         "unit_type": "currency",
     },
     "capacity_charge": {
         "text": "Capacity Charge",
         "description": "Fixed price charged per maximum utilized flowrate",
-        "unit_type": "currency",
+        "unit_type": "capacity_charge",
     },
     "co2_emission_per_unit": {
         "text": "Emissions Caused",
-        "description": "Caused Emissions in [kg CO2/unit]",
+        "description": "Caused Emissions",
         "unit_type": "emissions",
     },
 }
@@ -177,18 +177,18 @@ storage_parameters = {
     },
     "soc_start": {
         "text": "SOC Start",
-        "description": "Initial state of Charge [%]",
+        "description": "Initial state of Charge",
         "unit_type": "%",
     },
     "leakage_time": {
         "text": "Absolute Leakage",
         "description": "Absolute Leakage over Time",
-        "unit_type": "%",
+        "unit_type": "leakage_time",
     },
     "leakage_soc": {
         "text": "Relative Leakage",
         "description": "Relative Leakage over Time",
-        "unit_type": "%",
+        "unit_type": "leakage_soc",
     },
 }
 thermalsystem_parameters = {
@@ -233,12 +233,10 @@ component_parameter_list = {
 class ParameterConfigItem(TwoLineIconListItem):
     parameter = StringProperty()
     value_description = StringProperty()
-    unit_type = StringProperty()
+    unit = StringProperty()
 
 
 # FUNCTIONS
-
-
 def update_config_tab(app):
     """
     This function updates the config tab to display the values for the asset currently selected within the session.
@@ -252,7 +250,8 @@ def update_config_tab(app):
     # switch to the correct screen of the component tab
     app.root.ids.asset_config_screens.current = "component_config"
     # clear the tab from all existing widgets
-    app.root.ids.screen_component_config.clear_widgets()
+    app.root.ids.screen_component_config_top.clear_widgets()
+    app.root.ids.screen_component_config_bottom.clear_widgets()
 
     # CREATE LAYOUT FOR GENERAL DEFINITION
     layout_definition = layout_component_definition()
@@ -269,11 +268,13 @@ def update_config_tab(app):
     # update description textfield
     layout_definition.ids.textfield_description.text = app.selected_asset["description"]
     # add layout to the tab
-    app.root.ids.screen_component_config.add_widget(layout_definition)
+    app.root.ids.screen_component_config_top.add_widget(layout_definition)
 
     # add Input/output-ratio specification for converters
     if asset_type == "converter":
-        app.root.ids.screen_component_config.add_widget(layout_converter_ratios())
+        app.root.ids.screen_component_config_bottom.add_widget(
+            layout_converter_ratios()
+        )
 
     # stop here if the component is a pool
     if asset_type == "pool":
@@ -286,13 +287,39 @@ def update_config_tab(app):
 
     # add required parameters
     for parameter_key, attribute_data in component_parameter_list[asset_type].items():
+        # specify the correct unit for the selected parameter
+        if attribute_data["unit_type"] == "flowrate":
+            unit = app.selected_asset["flowtype"].unit.get_unit_flowrate()
+        elif attribute_data["unit_type"] == "flow":
+            unit = app.selected_asset["flowtype"].unit.get_unit_flow()
+        elif attribute_data["unit_type"] == "%":
+            unit = "%"
+        elif attribute_data["unit_type"] in ["float", "integer"]:
+            unit = "Units"
+        elif attribute_data["unit_type"] == "efficiency_drop":
+            unit = "%/Unit"
+        elif attribute_data["unit_type"] == "timesteps":
+            unit = "hours"
+        elif attribute_data["unit_type"] == "ramping":
+            unit = f"{app.selected_asset['flowtype'].unit.get_unit_flowrate()}/h"
+        elif attribute_data["unit_type"] == "currency":
+            unit = app.blueprint.info["currency"]
+        elif attribute_data["unit_type"] == "leakage_time":
+            unit = f"{app.selected_asset['flowtype'].unit.get_unit_flow()}/h"
+        elif attribute_data["unit_type"] == "leakage_soc":
+            unit = "%/h"
+        elif attribute_data["unit_type"] == "emissions":
+            unit = f"kgCO2/{app.selected_asset['flowtype'].unit.get_unit_flow()}"
+        elif attribute_data["unit_type"] == "capacity_charge":
+            unit = f"{app.blueprint.info['currency']}/{app.selected_asset['flowtype'].unit.get_unit_flowrate()}_peak"
+
         list_icon = IconLeftWidgetWithoutTouch(id="icon", icon="help")
         list_item = ParameterConfigItem(
             list_icon,
             id=f"attribute_{parameter_key}",
             text=attribute_data["text"],
             secondary_text="Not Specified",
-            unit_type=attribute_data["unit_type"],
+            unit=unit,
         )
         list_item.parameter = parameter_key
         list_item.value_description = attribute_data["description"]
@@ -322,7 +349,7 @@ def update_config_tab(app):
             elif len(parameters[parameter_key]) == 1:
                 value = parameters[parameter_key][next(iter(parameters[parameter_key]))]
                 if value["type"] == "static":
-                    list_item.secondary_text = f"{value['value']} {app.selected_asset['flowtype'].unit.get_unit_flowrate()}"
+                    list_item.secondary_text = f"{value['value']} {unit}"
                     list_icon.icon = "ray-start-arrow"
                 else:
                     list_item.secondary_text = f"Timeseries: {value['value']}"
@@ -342,8 +369,9 @@ def update_config_tab(app):
                 )
             )
         layout_config.ids.list_attributes.add_widget(list_item)
+
     # add parameter definitions to the layout
-    app.root.ids.screen_component_config.add_widget(layout_config)
+    app.root.ids.screen_component_config_bottom.add_widget(layout_config)
 
     # since the asset just got updated there are no unsaved changes anymore
     app.unsaved_changes_on_asset = False
