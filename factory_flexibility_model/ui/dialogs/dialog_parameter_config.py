4c8823b629021cfa22bf94ff8165d554d969981b
smsikamm <simon.kammerer@tu-dortmund.de>
2023-10-31 17:20:49 +0100

    refactor: sorted functions alphabetically

--- factory_flexibility_model/ui/dialogs/dialog_parameter_config.py
+++ factory_flexibility_model/ui/dialogs/dialog_parameter_config.py
@@ -27,7 +27,7 @@ class PopupAddTimeseries(BoxLayout):
 # FUNCTIONS
 def add_static_parameter_value(app):
     """
-    this function takes the value currently given by the user via the textfield_value and appends it to the parameter list of the component within app.session_data["parameters"]
+    This function takes the value currently given by the user via the textfield_value and appends it to the parameter list of the component within app.session_data["parameters"]
     """
 
     # get the value to store
@@ -80,6 +80,80 @@ def add_timeseries_parameter_value(app):
     update_parameter_value_list(app)
 
 
+def delete_parameter_value(app, value_key):
+    """
+    This function takes a pointer to a currently running app and a key to one of the values in the currently opened parametervalue definitiondialog. It deletes the referenced parameter from the app.session_data["parameters"] dict and refreshes the value list in the dialog.
+    :param app: Pointer to the currently running app instance
+    :param value_key: [str] Key to one value within the parameterlist of the selected asset
+    :return: true
+    """
+    del app.session_data["parameters"][app.selected_asset["key"]][app.dialog.parameter][
+        value_key
+    ]
+    update_parameter_value_list(app)
+
+
+def delete_timeseries(app, timeseries_key):
+    """
+    This function deletes the timeseries referenced by timeseries_key from app.session_data["timeseries"]
+    :param timeseries_key: [str] key to one of the items of app.session_data["timeseries"]
+    """
+    del app.session_data["timeseries"][timeseries_key]
+    update_timeseries_list(app)
+
+
+def select_timeseries_list_item(app, list_item, touch):
+    """
+    This function is called everytime when the user selects a timeseries in the scenarioparameter definition.
+    The selected timeseries is displayed in the preview window
+    """
+
+    # check, if the function call actually came from an object that has been clicked/moved
+    if not list_item.collide_point(*touch.pos):
+        # abort if not
+        return
+
+    # write the name of selected timeseries in the GUI
+    app.popup.content_cls.ids.label_selected_timeseries.text = list_item.text
+
+    # get data for selected timeseries and write it into self.selected_timeseries
+    app.popup.selected_timeseries = list_item.text
+
+    # write the values of the current timeseries into dialog.timeseries
+    app.popup.timeseries = np.array(
+        app.session_data["timeseries"][app.popup.selected_timeseries]["values"]
+    )
+
+    app.popup.content_cls.ids.button_add_timeseries.disabled = False
+
+    # update preview
+    update_timeseries_preview(app)
+
+
+def show_parameter_config_dialog(app, caller):
+    """
+    This function opens the dialog for configuring values of component parameters.
+    """
+
+    app.dialog = MDDialog(
+        title=f"{app.selected_asset['name']}: {caller.text}",
+        type="custom",
+        content_cls=DialogParameterConfig(),
+    )
+    app.dialog.size_hint = (None, None)
+    app.dialog.width = dp(650)
+    app.dialog.height = dp(900)
+
+    app.dialog.parameter_description = f"{caller.value_description} [{caller.unit}]"
+
+    # get the parameter to be configured with the opened dialog
+    app.dialog.parameter = caller.parameter
+
+    app.dialog.unit = caller.unit
+    update_parameter_value_list(app)
+    app.dialog.open()
+
+
 def show_popup_add_static_value(app):
     """
     This function creates a new dialog within app.popup. The Dialog Layout is taken from ui.dialogs.dialog_add_static_value.kv.
@@ -123,43 +197,6 @@ def show_popup_add_timeseries(app):
 
     # open popup
     app.popup.open()
-
-
-def show_parameter_config_dialog(app, caller):
-    """
-    This function opens the dialog for configuring values of component parameters.
-    """
-
-    app.dialog = MDDialog(
-        title=f"{app.selected_asset['name']}: {caller.text}",
-        type="custom",
-        content_cls=DialogParameterConfig(),
-    )
-    app.dialog.size_hint = (None, None)
-    app.dialog.width = dp(650)
-    app.dialog.height = dp(900)
-
-    app.dialog.parameter_description = f"{caller.value_description} [{caller.unit}]"
-
-    # get the parameter to be configured with the opened dialog
-    app.dialog.parameter = caller.parameter
-
-    app.dialog.unit = caller.unit
-    update_parameter_value_list(app)
-    app.dialog.open()
-
-
-def delete_parameter_value(app, value_key):
-    """
-    This function takes a pointer to a currently running app and a key to one of the values in the currently opened parametervalue definitiondialog. It deletes the referenced parameter from the app.session_data["parameters"] dict and refreshes the value list in the dialog.
-    :param app: Pointer to the currently running app instance
-    :param value_key: [str] Key to one value within the parameterlist of the selected asset
-    :return: true
-    """
-    del app.session_data["parameters"][app.selected_asset["key"]][app.dialog.parameter][
-        value_key
-    ]
-    update_parameter_value_list(app)
 
 
 def update_parameter_value_list(app):
@@ -251,34 +288,6 @@ def update_timeseries_list(app):
         app.popup.content_cls.ids.list_timeseries.add_widget(item)
 
 
-def select_timeseries_list_item(app, list_item, touch):
-    """
-    This function is called everytime when the user selects a timeseries in the scenarioparameter definition.
-    The selected timeseries is displayed in the preview window
-    """
-
-    # check, if the function call actually came from an object that has been clicked/moved
-    if not list_item.collide_point(*touch.pos):
-        # abort if not
-        return
-
-    # write the name of selected timeseries in the GUI
-    app.popup.content_cls.ids.label_selected_timeseries.text = list_item.text
-
-    # get data for selected timeseries and write it into self.selected_timeseries
-    app.popup.selected_timeseries = list_item.text
-
-    # write the values of the current timeseries into dialog.timeseries
-    app.popup.timeseries = np.array(
-        app.session_data["timeseries"][app.popup.selected_timeseries]["values"]
-    )
-
-    app.popup.content_cls.ids.button_add_timeseries.disabled = False
-
-    # update preview
-    update_timeseries_preview(app)
-
-
 def update_timeseries_preview(app):
     """
     This function creates a graph of a timeseries and hands it over to the preview canvas together with some general info on the selected timeseries to be displayed within the info labels of the dialog.
@@ -339,15 +348,6 @@ def update_timeseries_preview(app):
         graph.ymax = 10**i * round(x + 0.1, 1)
 
         graph.y_ticks_major = 10**i / 5
-
-
-def delete_timeseries(app, timeseries_key):
-    """
-    This function deletes the timeseries referenced by timeseries_key from app.session_data["timeseries"]
-    :param timeseries_key: [str] key to one of the items of app.session_data["timeseries"]
-    """
-    del app.session_data["timeseries"][timeseries_key]
-    update_timeseries_list(app)
 
 
 def validate_value(app, textfield):
