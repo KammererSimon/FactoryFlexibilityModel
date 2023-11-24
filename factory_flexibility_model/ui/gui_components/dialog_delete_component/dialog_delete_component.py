9f736258059a9f09484b5a4a5391fd1defa72e89
smsikamm <simon.kammerer@tu-dortmund.de>
2023-11-24 15:10:36 +0100

    refactor: splitted the delete component functionality into two skripts: one responsible for the gui within .gui_components and one for the functionality in .utility

--- factory_flexibility_model/ui/gui_components/dialog_delete_component/dialog_delete_component.py
+++ factory_flexibility_model/ui/gui_components/dialog_delete_component/dialog_delete_component.py
@@ -1,70 +1,12 @@
 # IMPORTS
 from kivymd.uix.button import MDFlatButton, MDRaisedButton
 from kivymd.uix.dialog import MDDialog
-from kivymd.uix.snackbar import Snackbar
 
-from factory_flexibility_model.ui.gui_components.layout_canvas.factory_visualisation import (
-    initialize_visualization,
+from factory_flexibility_model.ui.utility.delete_component import (
+    delete_selected_component,
 )
-from factory_flexibility_model.ui.utility.window_handling import close_dialog
-
 
 # FUNCTIONS
-def delete_selected_component(app):
-    """
-    This function deletes the component that is currently selected within the app.
-    The component to be deleted is defined through the current entry of app.selected_asset.
-    Deleting the component consists of three steps:
-
-    1) At first, the function goes through all connections within app.blueprint.connections and deletes all instances that involve the selected component as origion or destination.
-
-    2) The selected component might already have some parameters defined within app.session_data["parameters"]. If there is a key for the component in this dict, the entry is being removed
-
-    3) The component itself is being deleted from app.blueprint.components
-
-    Finally the GUI is resettet by setting app.selected_asset to None, calling the welcome screen on the right side and calling app.initialize_visualization() to redraw the system layout without the deleted component
-
-    :param app: Pointer to the main factory_GUIApp-Object
-    :return: None
-    """
-    close_dialog(app)
-
-    # check, which connections need to be removed along with the component
-    delete_list = []
-    for connection in app.blueprint.connections:
-        if (
-            app.blueprint.connections[connection]["from"] == app.selected_asset["key"]
-        ) or (app.blueprint.connections[connection]["to"] == app.selected_asset["key"]):
-            delete_list.append(connection)
-
-    # delete all parameters saved for the component
-    del app.session_data["parameters"][app.selected_asset["key"]]
-
-    # make sure that no connection to the component is set as a primary flow at any converter
-    for component in app.blueprint.components.values():
-        if component["type"] == "converter":
-            if component["GUI"]["primary_flow"] == app.selected_asset["key"]:
-                component["GUI"]["primary_flow"] = None
-
-    # delete the component
-    for connection in delete_list:
-        Snackbar(
-            text=f"Connection {app.blueprint.connections[connection]['name']} has been deleted!"
-        ).open()
-        del app.blueprint.connections[connection]
-
-    # inform the user
-    Snackbar(text=f"{app.selected_asset['name']} has been deleted!").open()
-
-    # delete the component out of the blueprint
-    del app.blueprint.components[app.selected_asset["key"]]
-
-    # now there is no more selected component
-    app.selected_asset = None
-    app.root.ids.asset_config_screens.current = "flowtype_list"
-
-    # redraw the visualisation without the selected component
-    initialize_visualization(app)
 
 
 def show_component_deletion_dialog(app):
