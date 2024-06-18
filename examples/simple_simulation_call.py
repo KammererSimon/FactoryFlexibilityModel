0c07fa8570dc2eee89ec765f449ef4308ba8ac72
Simon <simon.kammerer@tu-dortmund.de>
2024-06-18 16:12:38 +0200

    feat: added some comments

--- examples/simple_simulation_call.py
+++ examples/simple_simulation_call.py
@@ -1,13 +1,36 @@
+# INFO
+"""
+- Die funktion unten hat die zwei hyperparameter "storage_size" und "grid_capacity" als hyperparameter.
+    Als Rückgabewert bekommst du die Gesamtkosten des Systems in €
+
+- Im Sachkontext gucken wir damit, wie effizient ein Abfallverwerter laufen kann wenn man ihm unterschiedlich viel
+Netzanschlusskapazität und Batteriespeicher zur Verfügung stellt.
+
+- Die funktion hat noch zwei hardegecodete variablen für die investkosten in den Speicher + Leistungspreise die für den Netzanschluss zu zahlen sind mit drin.
+    -> Die brauchen wir für den gesamtkostenterm damit wir in der optimierung nicht einfach for free beliebig viel speichern nutzen dürfen...
+
+- Das betrachtete Simulationslayout liegt unter examples/Demo.
+    Wenn du dir das im Detail angucken willst: poetry run gui -> Demo.ffm in der gui öffnen
+
+- Wenn du dir ein simulationsergebnis im Detail angucken willst setzt den parameter "show_result" auf true, dann wird kein ergebnis zurückgegeben sondern stattdessen lokal das dashboard mit den ergebnissen des runs gehostet
+    -> Kannste dir mal angucken um mal n gefühl zu bekommen was die simulation im detail so macht.
+
+- Standardmäßig läuft das skript jetzt einfach still und leise durch. wenn du Wissen willst was passiert kannste das logginglevel verändern und in dem simulationsaufruf das solverlog aktivieren
+
+"""
+
+# IMPORTS
 import logging
 import os
 import factory_flexibility_model.factory.Blueprint as bp
 import factory_flexibility_model.simulation.Scenario as sc
 from factory_flexibility_model.simulation import Simulation as fs
 
-def simulate(storage_size: float = 0,
+# CODE
+def simulate(storage_size: float = 500,
              grid_capacity: float=500,
              session_folder: str ="examples/Demo",
-             show_results: bool = True):
+             show_results: bool = False):
 
     """
     This function performs a single simulation run and returns the total cost value as a result.
@@ -18,8 +41,8 @@ def simulate(storage_size: float = 0,
     """
 
     # define capex constants
-    capex_storage = 10          # Annual capital and depreciation cost of battery storages in [€/kWh/a]
-    capex_grid_capacity = 220   # Annual capacity charge for utilization of the powergrid in [€/kW/a]
+    capex_storage = 1          # Monthly capital and depreciation cost of battery storages in [€/kWh/month]
+    capex_grid_capacity = 20   # Montly capacity charge for utilization of the powergrid in [€/kW/month]
 
     # set logging level to avoid any unnecessary console outputs from the simulation scripts
     logging.basicConfig(level=logging.ERROR)
@@ -46,13 +69,11 @@ def simulate(storage_size: float = 0,
     # run simulation
     simulation.simulate(threshold=0.000001, solver_config={"log_solver": False, "mip_gap": 0.01})
 
-    # calculate costs:
-    capex = capex_storage * storage_size + capex_grid_capacity * grid_capacity
-    opex = simulation.result["objective"]
-
     if show_results:
         simulation.create_dash()
     else:
-        print(capex+opex)
+        # calculate and return costs:
+        capex = capex_storage * storage_size + capex_grid_capacity * grid_capacity
+        opex = simulation.result["objective"]
         return capex + opex
 
